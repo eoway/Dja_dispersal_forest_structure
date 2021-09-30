f083a4b9cb2d65df753a33b4f5a2ebcf911ace4d
Elsa Ordway <elsa.ordway@gmail.com>
2021-09-30 14:34:26 -0700

    inserted test file in test folder

---
+++ Code/Test/load_kmz.R
@@ -0,0 +1,18 @@
+#------------------------------------------------------------------------------------------------#
+setwd("G:/My Drive/GEDI/Southern_Sierra") # Google Drive
+#------------------------------------------------------------------------------------------------#
+# Set output dir for downloading and reading files
+# outdir="G:/My Drive/GEDI/Southern_Sierra"
+
+library(raster); library(rgdal); library(sp)
+library(reshape2); library(dplyr); library(rgeos)
+library(GISTools); library(sf); library(SDMTools)
+library(ggplot2); library(RColorBrewer); library(grid)
+library(rasterVis); library(viridis); library(stringr)
+library(cowplot); library(ggplotify); library(purrr)
+library(fasterize)
+
+ctaLines <- readOGR( dsn = paste(getwd()
+                                  , "Constrained_MAXI.kml"
+                                  , sep = "/"), 
+                     stringsAsFactors = FALSE)
