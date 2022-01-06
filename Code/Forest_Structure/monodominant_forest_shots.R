a81145172172fb7345ccc194282b756b7165c7aa
missaminahm <85972394+missaminahm@users.noreply.github.com>
2022-01-06 09:41:33 -0800

    updated for loop code
    level1B

--- Code/Forest_Structure/monodominant_forest_shots.R
+++ Code/Forest_Structure/monodominant_forest_shots.R
@@ -13,6 +13,22 @@ download.dir="C:/Users/missa/Downloads/monodominant_shots"
 files.GEDI02_A<-list.files(download.dir,pattern="*GEDI02_A*",recursive = F,full.names=T)
 files.GEDI02_A<-files.GEDI02_A[grepl('.h5$',files.GEDI02_A)]
 
+files.GEDI01_B <- list.files(download.dir,pattern="*GEDI01_B*",recursive = F,full.names=T)
+files.GEDI01_B<-files.GEDI01_B[grepl('.h5$',files.GEDI01_B)]
+
+WREF_bb <- c(45.8977, 45.7792, -122.0983, -121.7786)
+
+listof1B <- list()
+for (b in 1:length(files.GEDI01_B)) {
+  gedilevel1b <-readLevel1B(level1Bpath = files.GEDI01_B[b])
+  level1BGeo   <- getLevel1BGeo(gedilevel1b)
+  #level1bGeo_sub <- subset(level1BGeo, latitude_bin0 < WREF_bb[1] & latitude_bin0 > WREF_bb[2] & longitude_bin0 > WREF_bb[3] & longitude_bin0 < WREF_bb[4])
+  #WREF_shot_numbers <- level1bGeo_sub$shot_number
+  listof1B[[b]] <-level1BGeo
+  clean_list1B <- listof1B[!sapply(listof1B, is.null)]
+  final_dat <- do.call(rbind, clean_list1B)
+}
+
 listofdfs <- list()
 for (ii in 1:length(files.GEDI02_A)){
   gedilevel2a <-readLevel2A(level2Apath = files.GEDI02_A[ii])
